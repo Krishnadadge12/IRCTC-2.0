27fa8f4c415c691e98d03d4cca568d61539272a8
divitasaikia12-gif <divitasaikia12@gmail.com>
2026-02-07 15:37:19 +0530

    Delete Booking_System directory

--- Booking_System/server/db/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2')
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'D2_92665_Divita',
-    password: 'manager',
-    database: 'mrc_test_db'})
-
-module.exports = pool
